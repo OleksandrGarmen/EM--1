b44e54acdecf5f7864a782af8eb334e8f04a20bc
Garmen <no-reply@garmen.ovh>
2025-01-31 20:09:43 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 
-
+/* TASK#1 | LEVEL #1*/
 /*
 const prompt = require('prompt-sync')();
 let login = prompt ("Введіть логін: ")
 let password = prompt ("Введіть ваш пароль: ")
 
 if(login === "admin" && password === "12345"){
-    console.log("Вітаю, вхід успішний))")
+    console.log("Вітаю, вхід успішний!))")
 }else{
-    console.log("Вибачте, доступ закритий!")
+    console.log("Вибачте, доступ закритий!((")
 }
 
 */
 
 
 
+
+
+/* TASK#2 | LEVEL #1*/
 /*
-const prompt = require('prompt-sync')();18
+const prompt = require('prompt-sync')();
 let age = prompt("Введіть ваш вік: ")
 if(age>=18 && age>=0){
     console.log("Доступ наданий!")
@@ -28,3 +31,23 @@ if(age>=18 && age>=0){
 
 
 
+
+
+/* TASK#3 | LEVEL #1*/
+/*
+const prompt = require('prompt-sync')();
+let price = prompt("Введіть ціну товару для розрахунку: ")
+let sale = 15
+let result = price + sale + (sale/100)
+console.log(result)
+*/
+
+
+
+
+
+
+
+/* TASK#4 | LEVEL #2*/
+const prompt = require('prompt-sync')();
+let email = prompt("Введіть вашу пошту: ")
