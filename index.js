9d83fd2fec3a0b29c0278cad8735a9a3a43df010
Garmen <no-reply@garmen.ovh>
2025-01-31 19:14:20 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+/*
+const prompt = require('prompt-sync')();
+let login = prompt ("Введіть логін: ")
+let password = prompt ("Введіть ваш пароль: ")
+
+if(login === "admin" && password === "12345"){
+    console.log("Вітаю, вхід успішний))")
+}else{
+    console.log("Вибачте, доступ закритий!")
+}
+
+*/
+
+
+
+/*
+const prompt = require('prompt-sync')();18
+let age = prompt("Введіть ваш вік: ")
+if(age>=18 && age>=0){
+    console.log("Доступ наданий!")
+}else{
+    console.log("Доступ обмеждено!")
+}
+
+*/
+
+
+
